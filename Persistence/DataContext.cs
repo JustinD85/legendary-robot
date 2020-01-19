cefdde72e1a695a71db02607ba79af2b5289cefb
Justin Duncan <18714169+JustinD85@users.noreply.github.com>
2020-01-19 09:20:10 -0500

    Update DataContext.cs

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -17,17 +17,18 @@ namespace Persistence
         */
 
         public DbSet<Value> Values { get; set; }
-        // All
+        // If enabled -- converts DB to use the single-table discriminator pattern. (all entities should inherit from AActor)
         // public DbSet<AActor> Actors { get; set; }
 
         // Generic
         // public DbSet<Building> Buildings { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Pawn> Pawns { get; set; }
-        public DbSet<KanjiAliveAPI> Kanji { get; set; }
+       
 
         //Specific
         // public DbSet<Potion> Potions { get; set; }
+        public DbSet<KanjiAliveAPI> Kanji { get; set; }
 
         //Sub-Type
     }
