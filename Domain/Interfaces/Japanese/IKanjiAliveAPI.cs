b0ab38f247f055210ba3078217d257c376dd6a2a
Justin Duncan <18714169+JustinD85@users.noreply.github.com>
2020-01-19 09:14:03 -0500

    Renamed API Methods

--- Domain/Interfaces/Japanese/IKanjiAliveAPI.cs
+++ Domain/Interfaces/Japanese/IKanjiAliveAPI.cs
@@ -14,33 +14,33 @@ namespace Domain.Interfaces.Japanese
     public class Kanji
     {
         public Guid Id { get; set; }
-        public string character { get; set; }
-        public Meaning meaning { get; set; }
-        public Strokes strokes { get; set; }
-        public Onyomi onyomi { get; set; }
-        public Kunyomi kunyomi { get; set; }
-        public Video video { get; set; }
+        public string Character { get; set; }
+        public Meaning Meaning { get; set; }
+        public Strokes Strokes { get; set; }
+        public Onyomi Onyomi { get; set; }
+        public Kunyomi Kunyomi { get; set; }
+        public Video Video { get; set; }
 
     }
 
     public class Radical
     {
         public Guid Id { get; set; }
-        public string character { get; set; }
-        public int strokes { get; set; }
-        public string image { get; set; }
-        public Position position { get; set; }
-        public Name name { get; set; }
-        public Meaning meaning { get; set; }
-        public List<Animation> animation { get; set; }
+        public string Character { get; set; }
+        public int Strokes { get; set; }
+        public string Image { get; set; }
+        public Position Position { get; set; }
+        public Name Name { get; set; }
+        public Meaning Meaning { get; set; }
+        public List<Animation> Animation { get; set; }
     }
 
     public class References
     {
         public Guid Id { get; set; }
-        public short grade { get; set; }
-        public short kodansha { get; set; }
-        public short classicNelson { get; set; }
+        public short Grade { get; set; }
+        public short Kodansha { get; set; }
+        public short ClassicNelson { get; set; }
     }
 
     public class Example
@@ -56,10 +56,10 @@ namespace Domain.Interfaces.Japanese
     public class Audio
     {
         public Guid Id { get; set; }
-        public string opus { get; set; }
-        public string aac { get; set; }
-        public string ogg { get; set; }
-        public string mp3 { get; set; }
+        public string Opus { get; set; }
+        public string Aac { get; set; }
+        public string Ogg { get; set; }
+        public string Mp3 { get; set; }
     }
 
 
@@ -67,66 +67,66 @@ namespace Domain.Interfaces.Japanese
     public class Position
     {
         public Guid Id { get; set; }
-        public string hiragana { get; set; }
-        public string romaji { get; set; }
-        public string icon { get; set; }
+        public string Hiragana { get; set; }
+        public string Romaji { get; set; }
+        public string Icon { get; set; }
     }
     public class Name
     {
         public Guid Id { get; set; }
-        public string hiragana { get; set; }
-        public string romaji { get; set; }
+        public string Hiragana { get; set; }
+        public string Romaji { get; set; }
     }
     public class Meaning
     {
         public Guid Id { get; set; }
-        public string english { get; set; }
+        public string English { get; set; }
     }
 
     public class Strokes
     {
         public Guid Id { get; set; }
-        public int count { get; set; }
-        public List<Timing> timings { get; set; }
-        public List<Image> images { get; set; }
+        public int Count { get; set; }
+        public List<Timing> Timings { get; set; }
+        public List<Image> Images { get; set; }
     }
     public class Timing
     {
         public Guid Id { get; set; }
-        public string time { get; set; }
+        public string Time { get; set; }
     }
 
     public class Image
     {
         public Guid Id { get; set; }
-        public string image { get; set; }
+        public string Url { get; set; }
     }
 
 
     public class Onyomi
     {
         public Guid Id { get; set; }
-        public string romaji { get; set; }
-        public string katakana { get; set; }
+        public string Romaji { get; set; }
+        public string Katakana { get; set; }
     }
 
     public class Kunyomi
     {
         public Guid Id { get; set; }
-        public string romaji { get; set; }
-        public string hiragana { get; set; }
+        public string Romaji { get; set; }
+        public string Hiragana { get; set; }
     }
     public class Video
     {
         public Guid Id { get; set; }
-        public string poster { get; set; }
-        public string mp4 { get; set; }
-        public string webm { get; set; }
+        public string Poster { get; set; }
+        public string Mp4 { get; set; }
+        public string Webm { get; set; }
     }
     public class Animation
     {
         public Guid Id { get; set; }
-        public string animation { get; set; }
+        public string SVG { get; set; }
     }
 
 }
